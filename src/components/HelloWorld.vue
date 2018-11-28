85b48e4fedf5a19a2fdc276b1cc2ef2357a4b003
chendatony31 <31647753@qq.com>
2018-11-28 22:50:46 +0800

    add transaction & contract

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,84 +4,142 @@
       <p v-if="!currentAccount">
         <button @click="login">Login</button>
       </p>
-      
+
       <p v-else>
         {{currentAccount}}
         <button @click="logout">Logout</button>
         <br>
         <br>
-          <button @click="send">send EOS to TokenPocket</button>
+        <button @click="send">send EOS to TokenPocket</button>
+        <br>
+        <br>
+        <button @click="airgrab">airgrab POOR</button>
+        <br>
+        <br>
+        <button @click="airgrabContract">another way to airgrab POOR</button>
       </p>
-      
     </div>
-    
   </div>
 </template>
 
 <script>
-
 import ScatterJS from "scatter-js/dist/scatter.esm";
-import Eos from 'eosjs';
+import Eos from "eosjs";
 
 const network = {
-    blockchain: "eos",
-    host: "mainnet.eoscanada.com",
-    port: 443,
-    protocol: "https",
-    chainId: "aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906"
+  blockchain: "eos",
+  host: "mainnet.eoscanada.com",
+  port: 443,
+  protocol: "https",
+  chainId: "aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906"
 };
 
 export default {
-  name: 'HelloWorld',
-  data () {
+  name: "HelloWorld",
+  data() {
     return {
-      currentAccount: '',
+      currentAccount: "",
       scatter: null,
       eosClient: null
-    }
+    };
   },
   created() {
     ScatterJS.scatter.connect("scatter-demo").then(connected => {
-        // User does not have Scatter Desktop, Mobile or Classic installed.
-        if(!connected) return false;
+      // User does not have Scatter Desktop, Mobile or Classic installed.
+      if (!connected) return false;
 
-        this.scatter = ScatterJS.scatter;
+      this.scatter = ScatterJS.scatter;
 
-        const requiredFields = { accounts:[network] };
+      const requiredFields = { accounts: [network] };
 
-        window.scatter = null;
+      window.scatter = null;
     });
   },
   methods: {
     login() {
-      const requiredFields = { accounts:[network] };
+      const requiredFields = { accounts: [network] };
 
-      this.scatter.getIdentity(requiredFields).then(() => {
-        const account = this.scatter.identity.accounts.find(x => x.blockchain === 'eos');
-        this.currentAccount = account.name;
-        const eosOptions = { expireInSeconds:60 };
+      this.scatter
+        .getIdentity(requiredFields)
+        .then(() => {
+          const account = this.scatter.identity.accounts.find(
+            x => x.blockchain === "eos"
+          );
+          this.currentAccount = account.name;
+          this.currentPermission = account.authority;
 
-        this.eosClient = this.scatter.eos(network, Eos, eosOptions);
-      }).catch(error => {
+          const eosOptions = { expireInSeconds: 60 };
+
+          this.eosClient = this.scatter.eos(network, Eos, eosOptions);
+        })
+        .catch(error => {
           alert(JSON.stringify(error));
-      });
+        });
     },
     logout() {
       this.scatter.forgetIdentity();
       this.currentAccount = null;
     },
     send() {
-      this.eosClient.transfer(this.currentAccount, 'itokenpocket', '0.0001 EOS', 'from scatter-demo').then(data => {
-        alert(JSON.stringify(data));
-      }).catch(error => {
-        alert(JSON.stringify(error));
-      })
+      this.eosClient
+        .transfer(
+          this.currentAccount,
+          "itokenpocket",
+          "0.0001 EOS",
+          "from scatter-demo"
+        )
+        .then(data => {
+          alert(JSON.stringify(data));
+        })
+        .catch(error => {
+          alert(JSON.stringify(error));
+        });
+    },
+    airgrab() {
+      this.eosClient
+        .transaction({
+          actions: [
+            {
+              account: "poormantoken",
+              name: "signup",
+              authorization: [
+                {
+                  actor: this.currentAccount,
+                  permission: this.currentPermission
+                }
+              ],
+              data: {
+                owner: this.currentAccount,
+                quantity: "0.0000 POOR"
+              }
+            }
+          ]
+        })
+        .then(data => {
+          alert("succeed");
+          console.log(data);
+        })
+        .catch(err => {
+          alert(err);
+        });
+    },
+    airgrabContract() {
+      this.eosClient.contract("poormantoken").then(contract => {
+        contract
+          .signup(this.currentAccount, "0.0000 POOR")
+          .then(data => {
+            alert("succeed");
+            console.log(data);
+          })
+          .catch(err => {
+            alert(err);
+          });
+      });
     }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 </style>
